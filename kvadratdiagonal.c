5e084d0dc1ff7b446d2723f67aba2127fdd3be45
Alexby03 <alexanderby03@gmail.com>
2023-08-31 15:01:25 +0200

    Updated assignments

--- kvadratdiagonal.c
+++ kvadratdiagonal.c
@@ -7,6 +7,24 @@ int main()
     printf("Storlek: ");
     scanf("%d", &size);
 
-    
+    for (int i = 0; i < size; i++)
+    {
+        
+        for (int j = 0; j < size; j++)
+        {
+                
+            if (j > size - (i + 2) && (j < size - i) && ())
+            {
+                printf("*");
+            } 
+            else 
+            {
+                printf("+");
+            }
+
+        }
+        printf("\n");
+
+    }
 
 }
