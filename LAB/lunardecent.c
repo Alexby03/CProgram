1ebfb8c98e0b4c24f9e317176b55ec261aaf09fd
Alexby03 <alexanderby03@gmail.com>
2023-09-05 14:45:44 +0200

    Added flowchart2 and cleanup on LAB1

--- LAB/lunardecent.c
+++ LAB/lunardecent.c
@@ -5,32 +5,22 @@ float velocity = -25.0f;
 int throttle = 0;
 
 float Velocity()
-    {
-        
-        float newVelocity;
+{
+    float newVelocity;
+    newVelocity = velocity + (0.1 * throttle - 1.5);
+    return newVelocity;
+}
 
-        newVelocity = velocity + (0.1 * throttle - 1.5);
-
-        return newVelocity;
-
-    }
-    
-    float Height()
-    {
-
-        float newHeight;
-
-        newHeight = height + velocity + ((0.1 * throttle - 1.5) / 2);
-
-        return newHeight;
-
-    }
+float Height()
+{
+    float newHeight;
+    newHeight = height + velocity + ((0.1 * throttle - 1.5) / 2);
+    return newHeight;
+}
 
 int main()
 {
-
     int time = 0;
-
     printf("Lunar decent challenge!\n");
     printf("You will pilot a lunar decent the last 250m.\n");
     printf("Each turn represent 1-second decent time.\n");
@@ -44,34 +34,22 @@ int main()
 
         if (throttle < 0 || throttle > 100)
         {
-
             printf("Throttle must be between 0 and 100.\n");
-
             continue;
-
         }
 
         height = Height();
         velocity = Velocity();
-        
         time++;
-
     }
 
     if (velocity < -2.0f)
     {
-
         printf("FAILED! Crash landing at %.1f m/s", velocity);
-
     } 
     else
     {
-
         printf("SUCCEEDED! Successful landing with a speed of %.1f m/s", velocity);
-
     }
-    
-
     return 0;
-
 }
