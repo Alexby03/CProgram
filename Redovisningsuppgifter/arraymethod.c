ff8685ba0cc2a84be8066ff034775a88bff26069
Alexby03 <alexanderby03@gmail.com>
2023-09-14 11:12:40 +0200

    Finished F7_3

--- Redovisningsuppgifter/arraymethod.c
+++ Redovisningsuppgifter/arraymethod.c
@@ -1,11 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void InputArray(int array[], int array_lenght)
+#define LENGTH 30
+
+void InputArraySort(int array[], int array_lenght)
 {
 
     for(int i = 0; i < array_lenght; i++)
     {
+        array[i] = (rand()%21 - 10);
+    }
+
+    int swapped;
+    do
+    {
+
+        swapped = 0;
+        for(int i = 1; i < array_lenght; i++)
+        {
+
+            if(array[i - 1] < array[i])
+            {
+
+                int temp = array[i - 1];
+                array[i - 1] = array[i];
+                array[i] = temp;
+                swapped = 1;
+            }
+
+        }
+
+    }while(swapped);
+
+    for(int i = 0; i < array_lenght; i++)
+    {
+
         printf("%d ", array[i]);
+
+    }
+
+}
+
+void InputArrayRand(int array[], int array_lenght)
+{
+
+    for(int i = 0; i < array_lenght; i++)
+    {
+
+        array[i] = (rand()%21-10);
+
+        printf("%d ", array[i]);
+
+    }
+    printf("\n");
+
+}
+
+void InputArrayDeterm(int array[], int array_lenght)
+{
+
+    for(int i = 0; i < array_lenght; i++)
+    {
+
+        printf("%d ", array[i]);
+
     }
     printf("\n");
 
@@ -14,11 +73,16 @@ void InputArray(int array[], int array_lenght)
 int main()
 {
 
-    int size = 4;
+    srand(time(NULL));
 
-    int ex_array[4] = {0, 1, 2, 3};
+    //int ex_array[LENGTH] = {0, 1, 2, 3, 4};
+    //InputArrayDeterm(ex_array, LENGTH);
 
-    InputArray(ex_array, size);
+    int ex_array[LENGTH];
+    
+    //InputArrayRand(ex_array, LENGTH);
+
+    InputArraySort(ex_array, LENGTH);
 
     return 0;
 
